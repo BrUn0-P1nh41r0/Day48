7ee6cf78954ba980fa5d83ba004a2474615ac1e4
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-03-06 17:56:47 +0000

    day 48 complete

--- main.py
+++ main.py
@@ -1,42 +1,73 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-#Keep Chrome browser open aafer program finishes
+#Keep Chrome browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.python.org")
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-#price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
-#price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
-#print(f"{price_dollar.text}.{price_cents.text}")
+cookie = driver.find_element(By.ID, value="cookie")
 
-#search_bar = driver.find_element(By.NAME, value="q")
-#print(search_bar.get_attribute("placeholder"))
-#button = driver.find_element(By.ID, value="submit")
-#print(button.size)
-#documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
-#print(documentation_link.text)
+items = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+item_ids = []
+for item in items:
+    item_ids.append(item.get_attribute("id"))
 
-#bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
-#print(bug_link.text)
+timeout = time.time()+5
+five_min = time.time()+60*5
 
-#Mine
-#menu = driver.find_elements(By.XPATH, value='//*[@id="content"]/div/section/div[2]/div[2]/div/ul')
-#print(menu[0].text)
+while True:
+    cookie.click()
 
-#Solution
-event_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
-event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
-events = {}
+    # Every 5 seconds:
+    if time.time() > timeout:
 
-for n in range(len(event_times)):
-    events[n] = {
-        "time": event_times[n].text,
-        "name": event_names[n].text,
-    }
-print(events)
+        # Get all upgrade <b> tags
+        all_prices = driver.find_elements(by=By.CSS_SELECTOR, value="#store b")
+        item_prices = []
 
-driver.quit()
+        # Convert <b> text into an integer price.
+        for price in all_prices:
+            element_text = price.text
+            if element_text != "":
+                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                item_prices.append(cost)
+
+        # Create dictionary of store items and prices
+        cookie_upgrades = {}
+        for n in range(len(item_prices)):
+            cookie_upgrades[item_prices[n]] = item_ids[n]
+
+        # Get current cookie count
+        money_element = driver.find_element(by=By.ID, value="money").text
+        if "," in money_element:
+            money_element = money_element.replace(",", "")
+        cookie_count = int(money_element)
+
+        # Find upgrades that we can currently afford
+        affordable_upgrades = {}
+        for cost, id in cookie_upgrades.items():
+            if cookie_count > cost:
+                affordable_upgrades[cost] = id
+
+        # Purchase the most expensive affordable upgrade
+        highest_price_affordable_upgrade = max(affordable_upgrades)
+        print(highest_price_affordable_upgrade)
+        to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
+
+        driver.find_element(by=By.ID, value=to_purchase_id).click()
+
+        # Add another 5 seconds until the next check
+        timeout = time.time() + 5
+
+    # After 5 minutes stop the bot and check the cookies per second count.
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element(by=By.ID, value="cps").text
+        print(cookie_per_s)
+        #break
+
+#driver.quit()
